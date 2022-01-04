e1c4b8be8084ab470f19d83d5e71081c9aacea15
kalyan mishra <55200919+thekalyan001@users.noreply.github.com>
2022-01-04 13:12:31 +0530

    Create 3. Count the subarrays having product less than k.cpp

---
+++ Day1-Day5 GOLDMAN_SACHS/3. Count the subarrays having product less than k.cpp
@@ -0,0 +1,31 @@
+//https://practice.geeksforgeeks.org/problems/count-the-subarrays-having-product-less-than-k1708/1/#
+/*
+n = 4, k = 10
+a[] = {1, 2, 3, 4}
+Output : 
+7
+Explanation:
+The contiguous subarrays are {1}, {2}, {3}, {4} 
+{1, 2}, {1, 2, 3} and {2, 3} whose count is 7.
+*/
+
+//Solution----------
+int countSubArrayProductLessThanK(const vector<int>& arr, int n, long long k) {
+        //number of all possible subarrays of an array of size N is N * (N + 1)/2. Let countSubarrays (N) = N * (N + 1)/2
+        long long res=0,prod=1;
+        long long left=0,right=0;
+        
+        while(right<n)
+        {
+            prod*=arr[right];
+            while(prod>=k) //if product is >=k then debar the left_Val & increment left 
+            {
+                prod/=arr[left];
+                left++;
+            }
+            //formula to get the subarray 
+            res+=right-left+1; //number of subarray, eg at index 0  (0-0+1=1 ele)
+            right++;
+        }
+        return res;
+    }
